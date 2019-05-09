a85a350e1907779c017f77d92cec000b4f5a7dcf
Willy Romão <willyr.goncalves@gmail.com>
2019-05-09 09:55:14 -0300

    new simple tmux configuration

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-git clone https://github.com/willyrgf/.tmux.git ~/.tmux &&
-  ln -s -f ~/.tmux/.tmux.conf ~/ &&
-  ln -s -f ~/.tmux/.tmux.conf.local ~/ &&
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&
-  ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+cp -pv .tmux.conf ~/.tmux.conf &&
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&
+    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
